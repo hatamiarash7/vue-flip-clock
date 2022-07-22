9c375c02195d522cb0643c0f453b380071d24978
Arash Hatami <hatamiarash7@gmail.com>
2022-07-22 17:38:57 +0430

    some improvements

--- src/index.js
+++ src/index.js
@@ -15,11 +15,13 @@ const plugin = {
 
 // To auto-install when vue is found
 let GlobalVue = null;
+
 if (typeof window !== "undefined") {
   GlobalVue = window.Vue;
 } else if (typeof global !== "undefined") {
   GlobalVue = global.Vue;
 }
+
 if (GlobalVue) {
   GlobalVue.use(plugin);
 }
